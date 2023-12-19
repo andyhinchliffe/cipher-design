7f1633f5a68422045a2182e406c99015e45314c9
Andy Hinchliffe <118820952+andyhinchliffe@users.noreply.github.com>
2023-12-19 09:03:32 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const innerCirc = document.getElementById("innerCirc")
+
+function spinWheel(){
+
+    innerCirc.style.animation ="spin 1s linear"
+}
+
+
+
+solveButton.addEventListener("click", spinWheel)
+
